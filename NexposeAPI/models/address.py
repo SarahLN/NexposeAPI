0833a202c95fefa59c38b24ca8005b653d4ad692
SarahLN <noles.sarah@gmail.com>
2018-08-23 20:17:24 -0500

    add error checking

--- NexposeAPI/models/address.py
+++ NexposeAPI/models/address.py
@@ -1,3 +1,6 @@
+from models.errors import InvalidTypeError
+
+
 class Address():
     """
     Class defining the Address model
@@ -13,8 +16,15 @@ class Address():
         :param ip: a string containing the IP address
         :param mac: a string containing the MAC address
         """
-        self.__ip = ip
-        self.__mac = mac
+        if type(ip) is str:
+            self.__ip = ip
+        else:
+            raise InvalidTypeError('Invalid type for variable "ip": expected string, got {0}'.format(type(ip)))
+
+        if type(mac) is str:
+            self.__mac = mac
+        else:
+            raise InvalidTypeError('Invalid type for variable "mac": expected string, got {0}'.format(type(mac)))
 
     @property
     def ip(self):
