29d8ec80d51ad904aeda332d023d3fa90cc135cb
SarahLN <noles.sarah@gmail.com>
2018-08-23 19:34:58 -0500

    add attribute class

---
+++ NexposeAPI/models/attribute.py
@@ -0,0 +1,27 @@
+class Attribute():
+    """
+    Class defining the Attribute model
+    """
+
+    __name = None     # string
+    __value = None    # string
+
+    def __init__(self, name=None, value=None):
+        """
+        Initialize an Attribute object
+
+        :param name: a string containing the name of the address
+        :param value: a string containing the value associated with the given name
+        """
+        self.__name = name
+        self.__value = value
+
+    @property
+    def name(self):
+        """Return the name"""
+        return self.__name
+
+    @property
+    def value(self):
+        """Return the value"""
+        return self.__value
