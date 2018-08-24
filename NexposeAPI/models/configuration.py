2884139eb38afe6e732ea5c7cc5d3c986dc73cbb
SarahLN <noles.sarah@gmail.com>
2018-08-23 20:14:17 -0500

    add error checking

--- NexposeAPI/models/configuration.py
+++ NexposeAPI/models/configuration.py
@@ -1,3 +1,6 @@
+from models.errors import InvalidTypeError
+
+
 class Configuration():
     """
     Class defining the Configuration model
@@ -13,8 +16,15 @@ class Configuration():
         :param name: a string containing the name of the address
         :param value: a string containing the value associated with the given name
         """
-        self.__name = name
-        self.__value = value
+        if type(name) is str:
+            self.__name = name
+        else:
+            raise InvalidTypeError('Invalid type for variable "name": expected string, got {0}'.format(type(name)))
+
+        if type(value) is str:
+            self.__value = value
+        else:
+            raise InvalidTypeError('Invalid type for variable "value": expected string, got {0}'.format(type(value)))
 
     @property
     def name(self):
